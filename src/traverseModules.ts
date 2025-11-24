a4103a6d95405a8919e8f805b6f562373317fd51
sandwich <kupriianovvv@gmail.com>
2025-11-25 01:44:13 +0300

    populate URLToModuleMap inside traverseModules function

--- src/traverseModules.ts
+++ src/traverseModules.ts
@@ -2,20 +2,30 @@ import { dirname } from "node:path";
 import { getSourceCodeByURL } from "./getSourceCodeByURL.ts";
 import { moduleResolve } from "./resolve.ts";
 import { searchRequireCalls } from "./searchRequireCalls.ts";
+import { addURLToModuleEntry } from "./addURLToModuleEntry.ts";
+import { emitURLToModuleMap } from "./emitURLToModuleMap.ts";
 
 export const traverseModules = (entryPointURL: string) => {
+  const URLToModuleMap = {};
   const sourceCode = getSourceCodeByURL(entryPointURL);
   const modulePaths = searchRequireCalls(sourceCode);
-  traverseModulesRecursive(modulePaths, dirname(entryPointURL));
+  traverseModulesRecursive(modulePaths, dirname(entryPointURL), URLToModuleMap);
+  emitURLToModuleMap(URLToModuleMap);
 };
-const traverseModulesRecursive = (modulePaths: string[], base: string) => {
+const traverseModulesRecursive = (
+  modulePaths: string[],
+  base: string,
+  URLToModuleMap: Record<string, Function>
+) => {
   const moduleURLs = modulePaths.map((modulePath) =>
     moduleResolve(modulePath, base)
   );
   moduleURLs.forEach((moduleURL) => {
     const sourceCode = getSourceCodeByURL(moduleURL);
-    const modulePaths = searchRequireCalls(sourceCode);
+    addURLToModuleEntry(moduleURL, sourceCode, URLToModuleMap);
 
-    traverseModulesRecursive(modulePaths, dirname(moduleURL));
+    const modulePaths = searchRequireCalls(sourceCode);
+    traverseModulesRecursive(modulePaths, dirname(moduleURL), URLToModuleMap);
   });
+  return modulePaths;
 };
