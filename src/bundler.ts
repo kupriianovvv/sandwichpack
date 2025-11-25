8e9f3f30df7518162a5673285d180f0fec271f85
sandwich <kupriianovvv@gmail.com>
2025-11-26 01:20:41 +0300

    call emitCustomRequire and addEntryPointInvoke inside bundle.ts

--- src/bundler.ts
+++ src/bundler.ts
@@ -1,5 +1,9 @@
 import { resolve } from "node:path";
 import { traverseModules } from "./traverseModules.ts";
+import { emitCustomRequire } from "./emitCustomRequire.ts";
+import { addEntryPointInvoke } from "./addEntrypointInvoke.ts";
 
 const entryPointURL = resolve(import.meta.dirname, "./index.js");
 traverseModules(entryPointURL);
+emitCustomRequire();
+addEntryPointInvoke(entryPointURL);
