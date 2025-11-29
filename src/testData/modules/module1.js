ec6b28f9a856c984b7ae314b8ecd4fbab7c45045
sandwich <kupriianovvv@gmail.com>
2025-11-29 22:05:23 +0300

    adds case to test module cache

--- src/testData/modules/module1.js
+++ src/testData/modules/module1.js
@@ -2,5 +2,7 @@ console.log("module1 called");
 
 module.exports.fromModule1 = "fromModule1";
 const module2 = require("./module2");
+const module4 = require("./nestedModules/ultraNestedModules/module4");
 module.exports.gotFromNestedRequire = module2;
 console.log("module2 from module1", module2);
+console.log("module4 from module1", module4);
